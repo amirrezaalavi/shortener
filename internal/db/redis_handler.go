a78befd312e12ef76cb0f799fdfc2b73bafec66e
alavi <alavi@bornarad.com>
2026-02-07 17:04:31 +0330

    initial adding of a redis database handler

--- internal/db/redis_handler.go
+++ internal/db/redis_handler.go
@@ -3,11 +3,18 @@ package database
 import (
 	"context"
 	"fmt"
-	//	"fmt"
 	"sync"
+	"time"
 
 	redis "github.com/redis/go-redis/v9"
 )
+
+type Entity struct {
+	//Id uint
+	ShortSuffix string
+	Url         string
+	TTL         uint64
+}
 
 var (
 	DBClient     *redis.Client
@@ -17,7 +24,7 @@ var (
 	CTX = context.Background()
 )
 
-func create_database() *redis.Client {
+func create_connection_database() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:8002",
 		Password: "",
@@ -32,6 +39,30 @@ func create_database() *redis.Client {
 
 	return rdb
 }
+func get_url_database(rdb *redis.Client, Suffix string) (url string) {
+	url, err := rdb.HGet(CTX, Suffix, "URL").Result()
+	if err != nil {
+		panic(err)
+	}
+	return url
+
+}
+
+func exists_Suffix_database(rdb *redis.Client, Suffix string) (exists bool) { // Check if a Suffix exists by querying the database
+	output := rdb.HGet(CTX, Suffix, "Created_Time")
+	if output.Err() != nil {
+		fmt.Println("Entity Exists")
+		return true
+	}
+	return false
+}
+func create_entity_database(rdb *redis.Client, Url string, Suffix string) (err error) {
+	output := rdb.HSet(CTX, Suffix, "URL", Url, "Suffix", Suffix, "Created_Time", time.Time.Unix(time.Now())) // I'll use Suffix as a hash identifier for easier deduplication
+	err = output.Err()
+	return err
+
+}
+
 func close_database(rdb *redis.Client) {
 	rdb.Close()
 }
